2e76c1ca6734115b76f704c7c5677a2493cf98b6
Reginaldo Morikawa <morikawa77@gmail.com>
2019-06-19 22:18:55 -0300

    First commit

---
+++ Assets/Scripts/Loading/loading.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+public class loading : MonoBehaviour
+{
+    public string TextoLoading;
+    public float lps, tempo;
+    int i;
+    public Text[] caixaTexto;
+    public Text textoAtual;
+     void Start()
+    {
+        TextoLoading = File.ReadAllText("Assets\\Texts\\loading.txt");
+
+        caixaTexto[0].text = "";
+        caixaTexto[1].text = "";
+    }
+    void Update()
+    {
+        lerTexto();
+    }
+    void lerTexto()
+    {
+        tempo += Time.deltaTime;
+        if (tempo > (1f/lps) && i < TextoLoading.Length)
+        {
+          
+            if (TextoLoading[i] == '-')
+            {
+                i++;
+                int novotext = TextoLoading[i];
+                novotext -= 48;
+             
+                textoAtual = caixaTexto[novotext];
+            }
+            else
+            {
+                textoAtual.text += TextoLoading[i];
+            }
+            tempo = 0;
+                i++;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Loading/preloader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Preloader : MonoBehaviour
+{
+    public int sceneIndex;
+
+    private AsyncOperation progress;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(LoadLevel());
+        }
+
+        if (progress != null)
+        {
+            Debug.Log(progress.progress);
+            if (progress.progress >= 0.9f && Input.GetKeyDown(KeyCode.Return))
+            {
+                progress.allowSceneActivation = true;
+            }
+        }
+    }
+
+    private IEnumerator LoadLevel()
+    {
+        progress = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Single);
+        progress.allowSceneActivation = false;
+        yield return null;
+        Debug.Log("Load is done");
+    }
+}
