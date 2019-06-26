22260ab5115ddced05a6e6137cd6984618478bab
Reginaldo Morikawa <morikawa77@gmail.com>
2019-06-26 10:35:05 -0300

    Botão para pesquisa

---
+++ Assets/Scripts/Inicio/BotaoPesquisa.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotaoPesquisa : MonoBehaviour
+{
+    public void OpenURL()
+    {
+        Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSfhzDNP79qttGCSAgJ41A-hqPfrK4SgOmyIsgHD4iSKu4bHlw/viewform");
+    }
+}
